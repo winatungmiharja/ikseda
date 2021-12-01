cb9975e6fbcf11103ed4c82141a4cb4b42256ef6
winatungmiharja <winatungmiharja@gmail.com>
2021-12-01 16:15:05 +0900

    Initial commit from Create Next App

---
+++ src/pages/404.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { RiAlarmWarningFill } from 'react-icons/ri';
+
+import Layout from '@/components/layout/Layout';
+import CustomLink from '@/components/links/CustomLink';
+import Seo from '@/components/Seo';
+
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <Seo templateTitle='Not Found' />
+
+      <main>
+        <section className='bg-dark'>
+          <div className='layout flex flex-col justify-center items-center min-h-screen text-center text-white'>
+            <RiAlarmWarningFill
+              size={60}
+              className='animate-flicker drop-shadow-glow text-yellow-300'
+            />
+            <h1 className='mt-8'>Page Not Found</h1>
+            <CustomLink className='mt-4' href='/'>
+              Back to Home
+            </CustomLink>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
